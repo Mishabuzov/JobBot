2e2e7edd6e1a32ea80247d21359660503c913124
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-05-10 14:48:51 -0700

    provided arguments

--- crawl_and_build/pipeline.py
+++ crawl_and_build/pipeline.py
@@ -1,6 +1,7 @@
 from crawl_and_build.vacancies_preprocessor import preprocess_vacancies
 from crawl_and_build.vacancies_indexer import create_search_structures
 from crawl_and_build.vacancies_crawler import crawl_vacancies
+from sys import argv as args
 
 def create_data_and_structures(crawl=False, preprocess=False, create_index=True):
     if crawl:
@@ -10,6 +11,16 @@ def create_data_and_structures(crawl=False, preprocess=False, create_index=True)
     if create_index:
         create_search_structures()
 
-# TODO: Add environments!
+
 if __name__ == '__main__':
-    create_data_and_structures()
+    # parse args
+    is_need_crawl, is_need_to_preprocess, is_need_to_index = True, True, True
+    if args[1].strip().lower() == 'false':
+        is_need_crawl = False
+    if args[2].strip().lower() == 'false':
+        is_need_to_preprocess = False
+    if args[3].strip().lower() == 'false':
+        is_need_to_index = False
+
+    # start pipeline
+    create_data_and_structures(is_need_crawl, is_need_to_preprocess, is_need_to_index)
